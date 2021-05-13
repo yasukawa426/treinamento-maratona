02a812878d95c19886b07ed89daaa50a8eb83d5e
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2021-05-13 15:25:35 -0300

    Exercicio maior.py  pronto

---
+++ maior.py
@@ -0,0 +1,9 @@
+numeros = input().split(' ')
+print(numeros)
+num1 = int(numeros[0])
+num2 = int(numeros[1])
+num3 = int(numeros[2])
+
+maior1 = (num1 + num2 + abs(num1 - num2))// 2
+maior2 = (num3 + maior1 + abs(maior1 - num3)) //2
+print(maior2, end="")
