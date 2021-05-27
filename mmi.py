f066905dfcdc734c5219711017587fcbc721a289
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2021-05-26 22:00:16 -0300

    mmi arrumado. Tinha lido o enunciado errado

--- mmi.py
+++ mmi.py
@@ -8,28 +8,10 @@ for t in range(T):
     conjuntos.append(notas)
 
 for i, vetor in enumerate(conjuntos):
-    print(str(vetor[0]) + " " + str(vetor[len(vetor)-1]))
-    #se for o ultimo vetor
-    if i == (len(conjuntos) - 1):
-        if (any(vetor.count(x) > 1 for x in vetor)):
-            print("S", end="")
-        else: 
-            print("N", end="")
+    menorNota = vetor[0]
+    maiorNota = vetor[len(vetor)-1]
+    print(str(menorNota) + " " + str(maiorNota))
+    if menorNota == maiorNota:
+        print("S", end="" if i == len(vetor)-1 else "\n")
     else:
-        if (any(vetor.count(x) > 1 for x in vetor)):
-            print("S")
-        else: 
-            print("N")
-    
-
-
-# for t in range(T):
-#     notas = input().split(" ")
-#     notas = [int(i) for i in notas]
-#     notas.sort()
-#     print(str(notas[0]) + " " + str(notas[len(notas)-1]))
-#     print()
-#     if (any(notas.count(x) > 1 for x in notas)):
-#         print("S", end="")
-#     else: 
-#         print("N", end="")
+        print("N", end="" if i == len(vetor)-1 else "\n")  
